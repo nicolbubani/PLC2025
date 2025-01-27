1484d856a2bd933ed53d0c4e1903d70ae36541a3
nicolbubani <210199273@aston.ac.uk>
2025-01-27 15:51:57 +0000

    Update task2.hs

--- lab2-functionalprimer/haskell/task2.hs
+++ lab2-functionalprimer/haskell/task2.hs
@@ -1,4 +1,8 @@
+ onePlusone x = 1+1
+
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusone
+    
